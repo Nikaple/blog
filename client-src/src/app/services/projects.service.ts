2a3334edc2b34abec3aa651476478b336ce9aff6
nikaple <nikaple@qq.com>
2017-07-03 14:59:34 +0800

    basic framework

---
+++ client-src/src/app/services/projects.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { ProjectInfo } from '../models/projectInfo';
+
+
+@Injectable()
+export class ProjectsService {
+  projectsInfo: ProjectInfo[];
+
+  constructor() { }
+
+  getAllProjects() {
+
+  }
+}
